ec6d41fdc45d0a4e7c509888bdeb42fae70e9e0c
Vlad Pirlog <avi.pirlog@gmail.com>
2020-10-05 16:02:54 +0300

    :wrench: Update config files

--- config/config.ts
+++ config/config.ts
@@ -11,3 +11,14 @@ export const DATABASE_PORT: number = parseInt(config().DATABASE_PORT, 10);
 
 export const APP_HOST = Deno.env.get('APP_HOST') || '127.0.0.1';
 export const APP_PORT = Deno.env.get('APP_PORT') || 1401;
+
+export const OAUTH = {
+  GITHUB: {
+    AUTHORIZATION_ENDPOINT: 'https://github.com/login/oauth/authorize',
+    TOKEN_ENDPOINT: 'https://github.com/login/oauth/access_token',
+    USER_INFO_ENDPOINT: 'https://api.github.com/user',
+    REDIRECT_URI: config().GITHUB_OAUTH_REDIRECT_URI,
+    CLIENT_ID: config().GITHUB_OAUTH_CLIENT_ID,
+    CLIENT_SECRET: config().GITHUB_OAUTH_CLIENT_SECRET
+  }
+}
